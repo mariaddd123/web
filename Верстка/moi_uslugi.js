ac989c519e4c86bed20dbcf06c412254b4254a32
mariaddd123 <137111104+mariaddd123@users.noreply.github.com>
2024-02-15 15:02:08 +0300

    Add files via upload

---
+++ Верстка/moi_uslugi.js
@@ -0,0 +1,75 @@
+const apiKey = "StZKwTscb8mOL3YAP0oyAvTiOF2jN4UC";
+const apiUrl = "https://calendarific.com/api/v2/holidays?&country=RU&year=2024&type=national&month=";
+
+const holidayInput = document.querySelector(".show_holidays input");
+const holidayButton = document.querySelector(".show_holidays button");
+const monthList = [];
+const holidays = document.querySelector(".holidays_info");
+const holidaysBlock = document.querySelector('.holidays');
+const errorText = document.querySelector(".error");
+
+function updateHolidaysInfo(data) {
+    console.log("updateHolidays", data);
+    const holidaysInfo=data.response.holidays;
+    holidaysInfo.forEach(function(holiday) {
+        console.log(holiday)
+        const name = holiday.name;
+        const date = holiday.date.iso;
+        const li = document.createElement("li");
+        li.innerHTML = 'Дата:' + ' ' + date + ' ' + 'Праздник:' + ' ' + name;
+        holidays.appendChild(li);
+    })
+
+
+  holidaysBlock.style.display = "block";
+  errorText.style.display = "none";
+}
+
+// Метод GET
+async function checkHolidays(month) {
+  try {
+    const response = await fetch(`${apiUrl}${month}&api_key=${apiKey}`, {
+      method: "GET",
+    });
+    if (response.status === 404) {
+      errorText.style.display = "block";
+      holidaysBlock.style.display = "none";
+    } else {
+      const data = await response.json();
+      console.log(data);
+      if (data.response.length !=0) {
+        updateHolidaysInfo(data);
+      } else {
+        errorText.style.display = "block";
+        errorText.innerText = "Праздники не найдены";
+      }
+
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+function updateHolidayList() {
+    const monthListElement = document.getElementById("month-list");
+    monthListElement.innerHTML = "";
+  
+    for (const monthName of monthList) {
+      const listItem = document.createElement("li");
+      listItem.textContent = monthName;
+      monthListElement.appendChild(listItem);
+    }
+  }
+
+function handleSearch() {
+  checkHolidays(holidayInput.value);
+  holidayInput.value = "";
+}
+
+holidayButton.addEventListener("click", handleSearch);
+holidayInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    handleSearch();
+  }
+});
+
